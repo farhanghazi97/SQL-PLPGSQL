da3695f8604be515386483a55eb42ce17f66b205
Farhan Ghazi <farhan.ghazi@macrovue.com.au>
2019-11-19 20:39:51 +1100

    updated query5 to take in default arg

--- query5.py
+++ query5.py
@@ -20,7 +20,11 @@ except:
     query = "SELECT * FROM q5b('" + arg + "')"
     cur.execute(query)
     results = cur.fetchall()
-
+    for tup in results:
+        sub_id, code, title, class_type, enrol_count, quota, tag = tup
+        percentage = float(enrol_count) / float(quota)
+        if percentage < 0.50:
+            print("{} {} is {}% full".format(class_type, tag.strip(), int(percentage * 100)))	
    
 cur.close()
 conn.close()
